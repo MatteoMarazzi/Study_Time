814ac7e932285abca05ee542e87b3ce021e22579
MatteoMarazzi <Mat.Marazzi1@stud.uniroma3.it>
2024-01-12 16:21:21 +0100

    Rettangoli con immaggini
    Aggiunte delle icone per i riquadri

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:app/pages/home_quiz_page.dart';
 import 'package:app/pages/tomato_method.dart';
 import 'package:app/util/home_options.dart';
@@ -7,8 +6,6 @@ import 'package:flutter/material.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
-
   final String title;
 
   @override
@@ -16,70 +13,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(175, 238, 238, 1.0),
-      appBar: AppBar(
-         
-        backgroundColor: const Color.fromRGBO(2, 67, 69, 1),
-
-
-         title: Stack(                  //nuova funzione che permette di sovrapposizionare
-          children : [                  //dei Widget, utilissimo per libertà di posizionamento
-            Center(
-             child : Text(
+        backgroundColor: const Color.fromRGBO(175, 238, 238, 1.0),
+        appBar: AppBar(
+            backgroundColor: const Color.fromRGBO(2, 67, 69, 1),
+            title: Stack(
+              //nuova funzione che permette di sovrapposizionare
+              children: [
+                //dei Widget, utilissimo per libertà di posizionamento
+                Center(
+                  child: Text(
                     widget.title,
                     style: TextStyle(color: Colors.white),
                   ),
-            ),
-            Positioned(                  //scelta di usare il positioned per infinita possibilita
-              right: 0,                  //di posizionamento. 
-              bottom: -9,
-              child: IconButton(
-            
-             onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => HomeQuizPage(title: "QUIZ"),
+                ),
+                Positioned(
+                  //scelta di usare il positioned per infinita possibilita
+                  right: 0, //di posizionamento.
+                  bottom: -9,
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeQuizPage(title: "QUIZ"),
+                        ),
+                      );
+                    },
+                    icon: Image.asset('assets/tomato.png'),
                   ),
-                );
-             },
-            icon: Image.asset(
-              'assets/tomato.png'
-
+                ),
+              ],
+            )),
+        body: Center(
+            child: Column(
+          children: [
+            SizedBox(height: 20),
+            RoundedRectangle(
+              color: Colors.black,
+              backgroundColor: Colors.red,
+              boxTitle: "POMODORO",
+              destinationPage: tomato(),
+              pathImage: 'assets/Tomato_2.png',
+              heightImage: 130,
+              weightImage: 250,
             ),
-           ),
-              ),
-            ],
-      )
-        ),
-
-
-      body: Center(
-      child : Column(
-        children: [
-          SizedBox(height: 20),
-         RoundedRectangle(
-          color: Colors.red,
-          backgroundColor: Colors.red,
-          boxTitle: "POMODORO",
-          destinationPage: tomato(),
-         ),
-        SizedBox(height: 20),
-         RoundedRectangle(
-          color: Colors.yellow,
-           backgroundColor:Colors.yellow,
-           boxTitle: "QUIZ",
-         destinationPage: HomeQuizPage(title: "QUIZ"),)
-        ],
-      )
-      )
-    );
+            SizedBox(height: 20),
+            RoundedRectangle(
+              color: Colors.black,
+              backgroundColor: Colors.yellow,
+              boxTitle: "QUIZ",
+              destinationPage: HomeQuizPage(title: "QUIZ"),
+              pathImage: 'assets/allarm_clock.png',
+              heightImage: 130,
+              weightImage: 250,
+            )
+          ],
+        )));
   }
 }
-
--- lib/util/home_options.dart
+++ lib/util/home_options.dart
@@ -1,12 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-
-
 class RoundedRectangle extends StatelessWidget {
-  final Color color;              //ANCORA NON IMPLEMENTATO, HO MESSO DI DEFAUL IL NERO.Sarebbe colore scritte
+  final Color
+      color; //ANCORA NON IMPLEMENTATO, HO MESSO DI DEFAUL IL NERO.Sarebbe colore scritte
   final Color backgroundColor; // Aggiungiamo il colore di sfondo
   final Widget destinationPage; // Correggo il tipo del parametro
   final String boxTitle;
+  final String pathImage;
+  final double heightImage;
+  final double weightImage;
 
   const RoundedRectangle({
     Key? key,
@@ -14,59 +17,76 @@ class RoundedRectangle extends StatelessWidget {
     required this.backgroundColor, // Aggiorniamo il costruttore
     required this.destinationPage, // Aggiorniamo il costruttore con il widget destinazione
     required this.boxTitle,
+    required this.pathImage,
+    required this.heightImage,
+    required this.weightImage,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => destinationPage), // Utilizziamo il widget destinazione per la navigazione
-        );
-      },
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15), 
-        child: Row(
-      children : [
-        Expanded(
-          child: Container(
-            width: 325, // Bordo esterno larghezza
-            height: 80, // Bordo esterno altezza
-            decoration: BoxDecoration(
-              color: backgroundColor, // Utilizziamo il colore di sfondo
-              border: Border.all(
-                color: Colors.black,
-                width: 3.0            
-              ),
-           borderRadius: BorderRadius.circular(15),
-        ),
-        child: Center(
-          child: Container(
-            width: 310, // Bordo interno larghezza
-            height: 30, // Bordo interno altezza
-            child: Center(
-              child: Text(
-                boxTitle,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20,
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    destinationPage), // Utilizziamo il widget destinazione per la navigazione
+          );
+        },
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: 500, // Bordo esterno larghezza
+                    height: 100, // Bordo esterno altezza
+                    decoration: BoxDecoration(
+                      color: backgroundColor, // Utilizziamo il colore di sfondo
+                      border: Border.all(color: Colors.black, width: 3.0),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+
+                    child: Stack(children: [
+                      Center(
+                        child: Container(
+                          width: 500, // Bordo interno larghezza
+                          height: 150, //Bordo interno larghezza
+                          decoration: BoxDecoration(
+                            color: backgroundColor,
+                            borderRadius: BorderRadius.circular(15),
+                          ), // Bordo interno altezza
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          boxTitle, //titolo assegnato da noi come parametro
+                          style: TextStyle(
+                            color: color,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                          right: //Se metti left (sta a sinistra della scritta) e viceversa
+                              190, //Serve per posizionare immagine (destra/sinistra)
+                          bottom: -20, //Serve per posizionare immagine(su/giù)
+                          child: Container(
+                              width:
+                                  weightImage, //Serve per modificare larghezza immagine
+                              height:
+                                  heightImage, //Serve per modificare altezza immagine
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(pathImage),
+                                    fit: BoxFit.contain),
+                              )))
+                    ]),
+                  ),
                 ),
-              ),
-            ),
-            decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ),
-        ),
-      ),
-        ),
-      ],
-      )
-      )
-    );
+              ],
+            )));
   }
 }
 
