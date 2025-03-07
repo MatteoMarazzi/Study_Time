905b7752dc64b2d71a42424aafafe18fd216109e
francescoluca <francescoluca14@gmail.com>
2025-03-07 17:06:38 +0100

    fix errore

--- lib/util/bottom_bar.dart
+++ lib/util/bottom_bar.dart
@@ -1,6 +1,5 @@
 import 'package:app/util/bar_button.dart';
 import 'package:app/pages/profile_page.dart';
-import 'package:app/pages/exam_page.dart';
 import 'package:app/pages/home_page.dart';
 import 'package:app/pages/tomato_method.dart';
 import 'package:flutter/material.dart';
@@ -35,11 +34,6 @@ class BottomBar extends StatelessWidget {
               color: Color.fromARGB(255, 0, 0, 0),
               icon: Icons.bar_chart_sharp,
               destination: ProfilePage(),
-            ),
-            BarButton(
-              color: Color.fromARGB(255, 0, 0, 0),
-              icon: Icons.folder,
-              destination: ExamPage(),
             ),
           ],
         ),
