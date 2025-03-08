430d891e660af9000502e1451a775c930b056b54
francescoluca <francescoluca14@gmail.com>
2025-03-08 15:57:32 +0100

    inizio modifica profilePage

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -10,16 +10,33 @@ class ProfilePage extends StatefulWidget {
 }
 
 class ProfilePageState extends State<ProfilePage> {
-  Future<String> getUserName() async {
+  Future<Map<String, String>> getUserData() async {
     User? user = FirebaseAuth.instance.currentUser;
     if (user != null) {
       DocumentSnapshot doc = await FirebaseFirestore.instance
           .collection('users')
           .doc(user.uid)
           .get();
-      return doc.exists ? doc['name'] : 'Utente';
+
+      if (doc.exists && doc.data() != null) {
+        var data = doc.data() as Map<String, dynamic>;
+        String userName = data.containsKey('name') ? data['name'] : 'Utente';
+
+        // Recupero e formatto la data di creazione
+        String accountCreationDate = 'Data non disponibile';
+        if (data.containsKey('createdAt')) {
+          Timestamp timestamp = data['createdAt'];
+          DateTime date = timestamp.toDate();
+          accountCreationDate = "${date.day}/${date.month}/${date.year}";
+        }
+
+        return {
+          'name': userName,
+          'createdAt': accountCreationDate,
+        };
+      }
     }
-    return 'Utente';
+    return {'name': 'Utente', 'createdAt': 'Data non disponibile'};
   }
 
   @override
@@ -34,8 +51,8 @@ class ProfilePageState extends State<ProfilePage> {
             right: screenSize.width * 0.04,
             top: screenSize.height * 0.04),
         child: Center(
-          child: FutureBuilder<String>(
-              future: getUserName(),
+          child: FutureBuilder<Map<String, String>>(
+              future: getUserData(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return Center(child: CircularProgressIndicator());
@@ -43,7 +60,8 @@ class ProfilePageState extends State<ProfilePage> {
                 if (snapshot.hasError) {
                   return Text("Errore nel caricamento");
                 }
-                String userName = snapshot.data ?? "Utente";
+                String userName = snapshot.data!['name']!;
+                String accountCreationDate = snapshot.data!['createdAt']!;
                 return Column(
                   mainAxisSize: MainAxisSize.max,
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -51,13 +69,40 @@ class ProfilePageState extends State<ProfilePage> {
                     const SizedBox(
                       height: 20,
                     ),
-                    Text(
-                      'Ciao ${userName} 👋',
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          //decoration: TextDecoration.underline,
-                          decorationThickness: 1),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 130,
+                          width: 130,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/default-avatar.avif'),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 15),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Ciao ${userName} 👋',
+                              style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                  //decoration: TextDecoration.underline,
+                                  decorationThickness: 1),
+                            ),
+                            Text(
+                              'Account creato il ${accountCreationDate}',
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  //decoration: TextDecoration.underline,
+                                  decorationThickness: 1),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                     const SizedBox(
                       height: 25,
@@ -99,62 +144,6 @@ class ProfilePageState extends State<ProfilePage> {
                             ),
                             SizedBox(
                               height: 15,
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      width: screenSize.width * 0.8,
-                      height: screenSize.height * 0.2,
-                      decoration: BoxDecoration(
-                        boxShadow: const [
-                          BoxShadow(
-                              color: Colors.black,
-                              offset: Offset(1, 0),
-                              spreadRadius: 1,
-                              blurRadius: 1,
-                              blurStyle: BlurStyle.normal),
-                        ],
-                        color: const Color.fromARGB(255, 230, 233, 235),
-                        border: Border.all(width: 2, color: Colors.black),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Center(
-                                child: Text(
-                              'DOMANDE DELLA SETTIMANA',
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w600,
-                                  decoration: TextDecoration.underline),
-                            )),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              'Corrette : 0',
-                              style: TextStyle(fontSize: 15),
-                            ),
-                            SizedBox(
-                              height: 15,
-                            ),
-                            Text(
-                              'Sbagliate : 0',
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            SizedBox(
-                              height: 15,
-                            ),
-                            Text(
-                              'Senza risposta : 0', //aggiunta contatore risposte corrette,sbagliate e senza risposta
-                              style: TextStyle(fontSize: 16),
                             )
                           ],
                         ),
